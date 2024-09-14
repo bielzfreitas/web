4b167c43cd828edab3774a21c5771703e327db86
bielzfreitas <freddie.freitas17@gmail.com>
2024-09-13 21:15:46 -0300

    first commit

---
+++ src/components/empty-goals.tsx
@@ -0,0 +1,24 @@
+//no react a imagem é uma importacao -> precisa colocar entre "{}" e dar um nome a variavel
+import logo from '../assets/logo-in-orbit.svg'
+import letsStart from '../assets/lets-start-illustration.svg'
+import { DialogTrigger } from './ui/dialog'
+import { Button } from './ui/button'
+import { Plus } from 'lucide-react'
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screem flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="letsStart" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className="size-4" />
+          Cadastrar Meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
+}
